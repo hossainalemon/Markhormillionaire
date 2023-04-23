9a7144bfb72306551ca0d7d499956a99be964522
Emon Hossain <hossainalemoncse@gmail.com>
2023-04-24 00:40:55 +0600

    2nd try

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -2,79 +2,118 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Deposit;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
 
 class DepositController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(['auth', 'verified']);
-    }
 
+   public function index(User $user)
+{
+    $deposits = Deposit::where('user_id', $user->id)->get();
+    $totalDeposits = Deposit::where('user_id', $user->id)->sum('amount');
+    return view('dashboard', compact('user', 'deposits', 'totalDeposits'));
+}
+
+
+    /**
+     * Show the form for creating a new deposit.
+     *
+     * @return \Illuminate\View\View
+     */
     public function create()
     {
-        return view('deposits.create');
+        return view('dashboard');
     }
 
+    /**
+     * Store a newly created deposit in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
         $request->validate([
-            'user_id' => 'required|integer',
-            'amount' => 'required|numeric|min:1',
+            'amount' => 'required|numeric',
+            'payment_method' => 'required|string|max:255',
+            'transaction_number' => 'required|unique:deposits',
         ]);
 
-        Deposit::create([
-            'user_id' => $request->user_id,
-            'amount' => $request->amount,
-            'status' => 'pending',
-        ]);
+        $deposit = new Deposit;
+        $deposit->user_id = auth()->id();
+        $deposit->amount = $request->amount;
+        $deposit->payment_method = $request->payment_method;
+        $deposit->transaction_number = $request->transaction_number;
+        $deposit->save();
 
-        return Redirect::back()->with('success', 'Deposit Request submitted successfully!');
+        return redirect()->route('dashboard')->with('success', 'Deposit created successfully.');
     }
 
-    public function index(User $user)
-    {
-        $deposits = $user->deposits()->orderByDesc('created_at')->get();
 
-        return view('deposits.index', compact('deposits'));
-    }
 
+    /**
+     * Display the specified deposit.
+     *
+     * @param  int  $id
+     * @return \Illuminate\View\View
+     */
     public function show($id)
     {
         $deposit = Deposit::findOrFail($id);
 
-        return view('deposits.show', compact('deposit'));
+        return view('dashboard', compact('deposit'));
     }
 
+    /**
+     * Show the form for editing the specified deposit.
+     *
+     * @param  int  $id
+     * @return \Illuminate\View\View
+     */
     public function edit($id)
     {
         $deposit = Deposit::findOrFail($id);
 
-        return view('deposits.edit', compact('deposit'));
+        return view('dashboard', compact('deposit'));
     }
 
+    /**
+     * Update the specified deposit in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function update(Request $request, $id)
     {
         $request->validate([
-            'status' => 'required|string|in:approved,rejected',
+        'amount' => 'required|numeric',
+        'payment_method' => 'required|in:bank,mobile_bank',
+        'transaction_number' => 'required|unique:deposits',
         ]);
 
         $deposit = Deposit::findOrFail($id);
-        $deposit->update(['status' => $request->status]);
+        $deposit->amount = $request->amount;
+        $deposit->payment_method = $request->payment_method;
+        $deposit->transaction_number = $validatedData['transaction_number'];
+        $deposit->save();
 
-        return Redirect::back()->with('success', 'Deposit status updated successfully!');
+        return redirect()->route('dashboard', $deposit->id)->with('success', 'Deposit updated successfully.');
     }
 
+    /**
+     * Remove the specified deposit from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function destroy($id)
     {
         $deposit = Deposit::findOrFail($id);
         $deposit->delete();
 
-        return Redirect::back()->with('success', 'Deposit deleted successfully!');
+        return redirect()->route('dashboard')->with('success', 'Deposit deleted successfully.');
     }
 }
-
