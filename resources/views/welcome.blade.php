87370f61c7063503a28e93e0df7caee467fff131
Emon Hossain <hossainalemoncse@gmail.com>
2023-04-24 16:06:46 +0600

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,11 @@
        <!-- slider -->
        <div class="slider">
            <div class="slider-content">
-           <p>HI</p>
+           <h1><span class="cyen normal">Come Earn</span><br>
+    <span class="mewron"> Get financial freedom </span><br>
+ <span class="orange">&</span> rule the world <span class="orange">!</span></h1>
+ <p class="slider-content-des"> Becoming a leader with Markhor will give you , Online earning, profits, Monthly non working, and can make you a brand in itself! </p>
+ <button class="primary-btn">Register Now</button>
        </div>
        <div class="slider-thumail">
            <img src="assets/media/header.png">
@@ -46,5 +50,104 @@
           </div>
    <!-- end::Header -->
 
+   <!-- Featured Products  -->
+   <div class="featured-products">
+       <!-- begin::title -->
+       <div class="title">
+           <h2 class="upcase">Featured Products</h2>
+           <div class="title-bar-light"></div>
+           <div class="title-bar-dark"></div>
+           
+       </div>
+       <!-- end::title -->
+
+       <!-- begin::products -->
+       <div class="products">
+           <!-- begin:: item -->
+           <div class="product-grid">
+           <div class="product-thumbnail">
+               <img src="assets/media/headphone.jpg">
+           </div>
+           <div class="product-content">
+               <h3>Headphones Bluetooth Style 3 Lavender</h3>
+               <p class="center">Extra comfortable over-ear headphones for better noise cancellation.</p>
+               <p>RS.4000.00</p>
+           </div>
+           <div class="addtocard"> <a href="#">Buy</a> </div>
+       </div>
+           <!-- end::item -->
+            <!-- begin:: item -->
+           <div class="product-grid">
+           <div class="product-thumbnail">
+               <img src="assets/media/headphone.jpg">
+           </div>
+           <div class="product-content">
+               <h3>Headphones Bluetooth Style 3 Lavender</h3>
+               <p class="center">Extra comfortable over-ear headphones for better noise cancellation.</p>
+               <p>RS.4000.00</p>
+           </div>
+           <div class="addtocard"> <a href="#">Buy</a> </div>
+       </div>
+           <!-- end::item -->
+            <!-- begin:: item -->
+           <div class="product-grid">
+           <div class="product-thumbnail">
+               <img src="assets/media/headphone.jpg">
+           </div>
+           <div class="product-content">
+               <h3>Headphones Bluetooth Style 3 Lavender</h3>
+               <p class="center">Extra comfortable over-ear headphones for better noise cancellation.</p>
+               <p>RS.4000.00</p>
+           </div>
+           <div class="addtocard"> <a href="#">Buy</a> </div>
+       </div>
+           <!-- end::item -->
+            <!-- begin:: item -->
+           <div class="product-grid">
+           <div class="product-thumbnail">
+               <img src="assets/media/headphone.jpg">
+           </div>
+           <div class="product-content">
+               <h3>Headphones Bluetooth Style 3 Lavender</h3>
+               <p class="center">Extra comfortable over-ear headphones for better noise cancellation.</p>
+               <p>RS.4000.00</p>
+           </div>
+           <div class="addtocard"> <a href="#">Buy</a> </div>
+       </div>
+           <!-- end::item -->
+       </div>
+       <!-- end::products -->
+   </div>
+   <!-- end::Featured Products  -->
+<!-- being::footer -->
+    <div class="footer">
+        <div class="logo">
+            <img src="assets/media/logo_f.png">
+        </div>
+        <div class="about">
+            <h4> About us</h4>
+            <ul>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+        <div class="help">
+             <h4> HELP</h4>
+            <ul>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+        </div>
+        <div class="social">
+            <h4> Social</h4>
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="copyright"><p>Copy right by Markhormillionaire | Design and develop by ze group</p></div>
+<!-- end::footer -->
 </body>
 </html>
